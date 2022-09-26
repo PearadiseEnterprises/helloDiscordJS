16ca78be2f14127a31c1184a2ac65b8a1ce4a2c7
Daniel Kuzmin <danielkuzmin@Daniels-MBP.localdomain>
2022-09-25 19:11:26 -0700

    setup linter

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,6 +1,6 @@
 import { constants } from "buffer";
 import { Client, ClientOptions } from "discord.js";
-import { TOKEN } from "src/constants/constants";
+import { TOKEN } from 'src/constants/constants';
 
 console.log("Bot is starting...");
 
