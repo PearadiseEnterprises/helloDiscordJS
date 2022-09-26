97e5485ae5b438a8c121d66fa3ada393a14123f3
Daniel Kuzmin <danielkuzmin@gmail.com>
2022-09-25 20:02:16 -0700

    Started framework for tests

--- src/Bot.ts
+++ src/Bot.ts
@@ -2,11 +2,14 @@ import { constants } from "buffer";
 import { Client, ClientOptions } from "discord.js";
 import { TOKEN } from './const/constants';
 
+import ready from "./listeners/ready";
+
 console.log("Bot is starting...");
 
 const client = new Client({
     intents: []
 });
+ready(client)
 client.login(TOKEN);
 
 console.log(client);
