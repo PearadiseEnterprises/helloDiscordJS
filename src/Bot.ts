fc027a711778c8a6acd6d6e42122db7140643b53
Daniel Kuzmin <danielkuzmin@Daniels-MBP.localdomain>
2022-09-25 17:26:06 -0700

    Inital commit, setting up repo

---
+++ src/Bot.ts
@@ -0,0 +1,12 @@
+import { constants } from "buffer";
+import { Client, ClientOptions } from "discord.js";
+import { TOKEN } from "src/constants/constants";
+
+console.log("Bot is starting...");
+
+const client = new Client({
+    intents: []
+});
+client.login(TOKEN);
+
+console.log(client);
