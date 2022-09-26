39a439b4b886863bd65c523eded5558de63952aa
Daniel Kuzmin <danielkuzmin@gmail.com>
2022-09-25 20:16:41 -0700

    modify entry point (bot)

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,7 +1,6 @@
 import { constants } from "buffer";
 import { Client, ClientOptions } from "discord.js";
 import { TOKEN } from './const/constants';
-
 import ready from "./listeners/ready";
 
 console.log("Bot is starting...");
